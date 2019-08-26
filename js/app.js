bb396b940a524647eea8436f5a10848ef4d8e7b7
Patrick Ly <patrickkimly@gmail.com>
2019-08-26 09:07:52 -0700

    Changed the search-icon into an x-icon on click

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // var menuBtn = document.getElementsByClassName('menu-btn');
 // var mobileMenu = document.getElementsByClassName('mobile-menu');
 var searchingInput = document.getElementsByClassName('searching');
-var searchIcon = document.getElementsByClassName('fa-search');
+var searchIcon = document.getElementsByClassName('search-icon');
+var faSearch = document.getElementsByClassName('fa-search');
+
 
 var clickedBtn = function() {
     // mobileMenu[0].classList.toggle('active');
     searchingInput[0].classList.toggle('act');
+    faSearch[0].classList.toggle('fa-times');
+
 }
 
 searchIcon[0].addEventListener('click', clickedBtn);

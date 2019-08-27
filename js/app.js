2a451295913159760ccbd25b62eef46c61581ddf
Patrick Ly <patrickkimly@gmail.com>
2019-08-27 08:57:11 -0700

    can use two search icons to toggle the search form

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 // var menuBtn = document.getElementsByClassName('menu-btn');
 // var mobileMenu = document.getElementsByClassName('mobile-menu');
 var searchingInput = document.getElementsByClassName('searching');
-var searchIcon = document.getElementsByClassName('search-icon');
-var xIcon = document.getElementsByClassName('fa-times');
-
+// var searchIcon = document.getElementsByClassName('search-icon');
+// var xIcon = document.getElementsByClassName('fa-times');
+var abc = document.getElementById("abc");
 
 var clickedBtn = function() {
     // mobileMenu[0].classList.toggle('active');
     searchingInput[0].classList.toggle('act');
-    searchIcon[0].classList.toggle('fa-times');
-    
+    // searchIcon[0].classList.toggle('fa-times');
+    // abc.classList.toggle('act');
+    console.log("clicked");
 }
 
 var logging = function (){
     
 }
-searchIcon[0].addEventListener('click', clickedBtn);
+abc.addEventListener('click', clickedBtn);
+
+console.log("abc: ", abc);
+// console.log('searchingInput:', searchIcon[0]);
 
 
-console.log('searchingInput:', searchIcon[0]);
+
+

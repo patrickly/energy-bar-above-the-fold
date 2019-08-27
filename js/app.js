b2343c495fa9c7b0de102d53ce1dc910989d20ab
Patrick Ly <patrickkimly@gmail.com>
2019-08-27 08:00:47 -0700

    work in progress for search icon js

--- js/app.js
+++ js/app.js
@@ -2,15 +2,20 @@
 // var mobileMenu = document.getElementsByClassName('mobile-menu');
 var searchingInput = document.getElementsByClassName('searching');
 var searchIcon = document.getElementsByClassName('search-icon');
-var faSearch = document.getElementsByClassName('fa-search');
+var xIcon = document.getElementsByClassName('fa-times');
 
 
 var clickedBtn = function() {
     // mobileMenu[0].classList.toggle('active');
     searchingInput[0].classList.toggle('act');
-    faSearch[0].classList.toggle('fa-times');
-
+    searchIcon[0].classList.toggle('fa-times');
+    
 }
 
+var logging = function (){
+    
+}
 searchIcon[0].addEventListener('click', clickedBtn);
-console.log('searchingInput:', searchingInput[0]);
+
+
+console.log('searchingInput:', searchIcon[0]);
